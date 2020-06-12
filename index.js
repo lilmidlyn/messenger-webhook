6034d070a41929e174057501ffa14a0b12f37590
Midlyn Chen <midlynchen@icloud.com>
2020-06-11 22:13:46 -0700

    update

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === 'get_started') {
-    response = { "text": "Oops, try sending another image." };
+    response = askTemplate('Are you ready to make a bet?')
     callSendAPI(sender_psid, response);
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
