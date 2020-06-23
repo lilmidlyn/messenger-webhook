4c47778b5ff926b8d3e343657e60ee7ab5b1759f
Midlyn Chen <midlynchen@icloud.com>
2020-06-23 00:06:18 -0700

    update

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function handlePostback(sender_psid, received_postback) {
     response = nonconfidentialResources('Here are some nonconfidential resources:')
   }
   else if (payload === 'notsure') {
-    response = underage("text": 'Does this work?')
+    response = notSure("text": 'Does this work?')
     //response = notSure('It is common after sexual assault to be confused about how to react. The following can be used as a guide to help you find support and resources. Sexual consent consists of underage sex or absence of voluntary consent for the entirety of the sexual encounter.')    
   }
   else if (payload === 'underage') {
