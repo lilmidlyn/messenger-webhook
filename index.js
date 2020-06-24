e2dfc50e36b796075002e0b24d0345b134416ad5
Midlyn Chen <midlynchen@icloud.com>
2020-06-23 23:58:41 -0700

    update

--- index.js
+++ index.js
@@ -151,10 +151,10 @@ function handlePostback(sender_psid, received_postback) {
     response = reportingoptions('Are you currently enrolled in school/university?')
   }
   else if (payload === 'inschool') {
-    response = reportingoptions('Often schools and universities have counselors and staff trained to help sexual assault victims. If you are currently enrolled in a university, your campus most likely has a Title IX office who can help you with filing a report both through the university and officially with the police. \n  However, there are also other nonaffiliated resources available as well.')
+    response = reportingresources('Often schools and universities have counselors and staff trained to help sexual assault victims. If you are currently enrolled in a university, your campus most likely has a Title IX office who can help you with filing a report both through the university and officially with the police. \n  However, there are also other nonaffiliated resources available as well.')
   }
   else if (payload === 'notinschool') {
-    response = reportingoptions('Are you currently enrolled in school/university?')
+    response = reportingresources('Are you currently enrolled in school/university?')
   }
   else if (payload === 'notsure') {
     response = notSure('It is common after sexual assault to be confused about how to react. The following can be used as a guide to help you find support and resources. Sexual consent consists of underage sex or absence of voluntary consent for the entirety of the sexual encounter.')    
@@ -173,6 +173,25 @@ function handlePostback(sender_psid, received_postback) {
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
+}
+
+const reportingresources = (text) => {
+  return {
+    "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"button",
+                "text": text,
+                "buttons":[
+                    {
+                        "type":"web_url",
+                        "url" : "https://centers.rainn.org/",
+                        "title":"Find RAINN help centers"
+                    }
+                ]
+            }
+        }
+    }
 }
 
 const reportingoptions = (text) => {
